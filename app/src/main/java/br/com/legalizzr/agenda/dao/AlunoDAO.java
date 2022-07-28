bb9063c450484d1784bf1fb66ba62fc74cc7b8ea
Wesley Pereira <wesley.pereira@southsystem.com.br>
2022-07-27 23:30:52 -0300

    Primeiro APP da formação android da alura

---
+++ app/src/main/java/br/com/legalizzr/agenda/dao/AlunoDAO.java
@@ -0,0 +1,19 @@
+package br.com.legalizzr.agenda.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.legalizzr.agenda.model.Aluno;
+
+public class AlunoDAO {
+
+    private final static List<Aluno> alunos = new ArrayList<>();
+
+    public void salva(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public List<Aluno> todos() {
+        return new ArrayList<>(alunos);
+    }
+}
